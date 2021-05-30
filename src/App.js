4903b3181eedf4075440f240672bc6cc4bbc1a41
unknown <marinrusic@yahoo.com>
2021-05-30 11:18:48 +0200

    fist commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import uuid from 'uuid';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import TodoInput from './components/TodoInput'
+import TodoList from './components/TodoList'
+
+
+function App() {
+  return (
+    <div className="App">
+     <TodoInput />
+     <TodoList />
+    </div>
+  );
+}
+
+export default App;
